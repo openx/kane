8e4377eb136a0b8f0c7c92ecd15a715f12fc246b
Jaebin Yoon <jaebin.yoon@openx.com>
2016-03-25 11:53:44 -0700

    version 0.0.9

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Kane.Mixfile do
 
   def project do
     [app: :kane,
-     version: "0.0.8",
+     version: "0.0.9",
      elixir: "~> 1.2",
      package: package,
      description: description,
